862d7ed1bacb9f4157557837132067268519d8a7
treybourgeois <trey@retroactivepedals.com>
2021-02-25 21:20:51 -0600

    Thursday 2/25 with Excel doc

--- data/app_data_project.sql
+++ data/app_data_project.sql
@@ -16,6 +16,44 @@ FROM app_store_apps
 group by primary_genre
 order by times_downloaded DESC; -- games by a lot/ 3,862
 
-SELECT install_count
-FROM play_store_apps;
+SELECT DISTINCT(p.name), p.rating, p.review_count, p.type, RTRIM(p.install_count,'+') as install_count, a.primary_genre, p.content_rating from play_store_apps as p
+INNER JOIN app_store_apps as a
+ON a.name = p.name
+WHERE p.rating >= 4.5 AND p.content_rating = 'Teen' AND p.review_count < 1000000000 and a.primary_genre = 'Games'
+GROUP BY p.name, p.rating, p.review_count, p.type, p.install_count, p.content_rating, a.primary_genre
+HAVING AVG(p.review_count) > 445000
+ORDER BY p.review_count DESC;-- Scott's Query
+
+SELECT a.name, a.rating
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.rating >= 4.0 AND p.rating >= 4.0
+GROUP BY a.name, a.rating, p.name, p.rating
+ORDER BY p.rating;
+
+SELECT DISTINCT name, rating, max(review_count) AS HIGHEST_REVIEW, category
+FROM play_store_apps
+WHERE rating IS NOT NULL and category = 'GAME'
+GROUP BY name, rating, category
+ORDER BY highest_review desc
+LIMIT 10;-- google
+
+SELECT DISTINCT name, rating, CAST(review_count AS INT) AS HIGHEST_REVIEW, primary_genre
+FROM app_store_apps
+WHERE rating IS NOT NULL AND primary_genre = 'Games'
+GROUP BY name, rating, primary_genre, highest_review
+ORDER BY highest_review desc
+LIMIT 10;-- apple
+
+SELECT DISTINCT a.name , a.rating AS apple_rating, CAST(a.review_count AS INT) AS apple_highest_reviews, a.primary_genre AS apple_genres, p.rating AS google_rating, p.review_count AS google_review_count, p.category AS google_genre
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.rating IS NOT NULL AND p.rating IS NOT NULL AND a.primary_genre = 'Games' AND p.category = 'GAME'
+GROUP BY a.name, a.rating, a.primary_genre, apple_highest_reviews, p.name, p.rating, p.review_count, p.genres, p.category
+ORDER BY p.review_count desc
+LIMIT 27; 
+
+
 
